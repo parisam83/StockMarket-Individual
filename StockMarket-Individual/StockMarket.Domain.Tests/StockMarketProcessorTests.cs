b56b23a750def749cc7743e35655b39c15b9429e
Parisa <mousaviparisa5@yahoo.com>
2023-05-23 23:00:28 +0330

    Add Class and Tests file

---
+++ StockMarket-Individual/StockMarket.Domain.Tests/StockMarketProcessorTests.cs
@@ -0,0 +1,11 @@
+namespace StockMarket.Domain.Tests
+{
+    public class StockMarketProcessorTests
+    {
+        [Fact]
+        public void EnqueueOrder_Should_Proccess_SellOrder_When_BuyOrder_Is_Already_Enqueued_Test()
+        {
+
+        }
+    }
+}
