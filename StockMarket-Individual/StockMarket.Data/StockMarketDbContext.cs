6248b251499ef547da4a66827a6dad117d4fbcc2
Parisa <mousaviparisa5@yahoo.com>
2023-05-25 19:50:57 +0330

    Add DbContext

---
+++ StockMarket-Individual/StockMarket.Data/StockMarketDbContext.cs
@@ -0,0 +1,23 @@
+﻿using StockMarket.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace StockMarket.Data
+{
+    public class StockMarketDbContext : DbContext
+    {
+        public StockMarketDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Order>(b =>
+            {
+                b.Property(o => o.Id);
+                b.Property(o => o.TradeSide);
+                b.Property(o => o.Price).HasColumnType("Money");
+                b.Property(o => o.Quantity).HasColumnType("Money");
+            });
+        }
+    }
+}
---
+++ StockMarket-Individual/StockMarket.Data/StockMarketDbContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace StockMarket.Data
+{
+    internal class StockMarketDbContextFactory : IDesignTimeDbContextFactory<StockMarketDbContext>
+    {
+        public StockMarketDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<StockMarketDbContext>();
+            optionsBuilder.UseSqlServer("server=.\\sqlexpress;database=StockMarket;MultipleActiveResultSets=true;trusted_connection=true;encrypt=yes;trustservercertificate=yes;");
+            return new StockMarketDbContext(optionsBuilder.Options);
+        }
+    }
+}
