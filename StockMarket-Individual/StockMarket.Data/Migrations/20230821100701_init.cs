10e3bad9cb88daf6e342cada4ec6df3450ef076a
Parisa <mousaviparisa5@yahoo.com>
2023-08-21 14:32:45 +0330

    Add Orders & Trades to StockMarketDbContext

---
+++ StockMarket-Individual/StockMarket.Data/Migrations/20230821100701_init.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StockMarket.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false),
+                    Price = table.Column<decimal>(type: "Money", nullable: false),
+                    Quantity = table.Column<decimal>(type: "Money", nullable: false),
+                    TradeSide = table.Column<int>(type: "int", nullable: false),
+                    IsCanceled = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Trades",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false),
+                    BuyOrderId = table.Column<long>(type: "bigint", nullable: false),
+                    SellOrderId = table.Column<long>(type: "bigint", nullable: false),
+                    Quantity = table.Column<decimal>(type: "Money", nullable: false),
+                    Price = table.Column<decimal>(type: "Money", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trades", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Trades_Orders_BuyOrderId",
+                        column: x => x.BuyOrderId,
+                        principalTable: "Orders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Trades_Orders_SellOrderId",
+                        column: x => x.SellOrderId,
+                        principalTable: "Orders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Trades_BuyOrderId",
+                table: "Trades",
+                column: "BuyOrderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Trades_SellOrderId",
+                table: "Trades",
+                column: "SellOrderId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Trades");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+        }
+    }
+}
--- StockMarket-Individual/StockMarket.Data/StockMarketDbContext.cs
+++ StockMarket-Individual/StockMarket.Data/StockMarketDbContext.cs
@@ -5,6 +5,9 @@ namespace StockMarket.Data
 {
     public class StockMarketDbContext : DbContext
     {
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Trade> Trades { get; set; }
+
         public StockMarketDbContext(DbContextOptions options) : base(options)
         {
         }
@@ -13,10 +16,18 @@ namespace StockMarket.Data
         {
             modelBuilder.Entity<Order>(b =>
             {
-                b.Property(o => o.Id);
+                b.Property(o => o.Id).ValueGeneratedNever();
                 b.Property(o => o.TradeSide);
                 b.Property(o => o.Price).HasColumnType("Money");
                 b.Property(o => o.Quantity).HasColumnType("Money");
+            });
+            modelBuilder.Entity<Trade>(b =>
+            {
+                b.Property(t => t.Id).ValueGeneratedNever();
+                b.HasOne<Order>().WithMany().IsRequired().HasForeignKey(t => t.SellOrderId).OnDelete(DeleteBehavior.Restrict);
+                b.HasOne<Order>().WithMany().IsRequired().HasForeignKey(t => t.BuyOrderId).OnDelete(DeleteBehavior.Restrict);
+                b.Property(t => t.Price).HasColumnType("Money");
+                b.Property(t => t.Quantity).HasColumnType("Money");
             });
         }
     }
